3c2f33f35f2ba56aaa6e2298ffd82026de47e513
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2025-01-10 13:11:04 -0800

    making methods for lab

---
+++ Vector3D.java
@@ -0,0 +1,52 @@
+public class Vector3D {
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector3D(double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public double getZ() {
+        return z;
+    }
+
+    public double getMagnitude(){
+        return Math.sqrt((x*x) + (y*y) +(z*z));
+    }
+
+    public Vector3D normalize() {
+        double magnitude = getMagnitude();
+        if (magnitude == 0) {
+            throw new IllegalStateException("Magnitude is zero");
+        } else {
+            return new Vector3D(x / magnitude, y / magnitude, z / magnitude);
+        }
+    }
+
+    public Vector3D add(Vector3D inputVector){
+        return new Vector3D(x + inputVector.getX(), y + inputVector.getY(), z + inputVector.getZ());
+    }
+
+    public Vector3D multiply(double multiplier){
+        return new Vector3D(x * multiplier, y * multiplier, z * multiplier);
+
+    }
+
+    public Vector3D dotProduct(Vector3D inputVector){
+        return new Vector3D(x * inputVector.getX(), y * inputVector.getY(), z * inputVector.getZ());
+    }
+
+@Override
+    public String toString(){
+        return("(" + x + "," + y + "," + "z" + ")");
+    }
+}
