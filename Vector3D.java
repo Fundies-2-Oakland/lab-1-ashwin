e09ab0d3dffaffbf5dcd3e98cbf74bd6b561a0a3
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2025-01-10 18:09:25 -0800

    make final

--- Vector3D.java
+++ Vector3D.java
@@ -1,7 +1,7 @@
 public class Vector3D {
-    private double x;
-    private double y;
-    private double z;
+    private final double x;
+    private final double y;
+    private final double z;
 
     public Vector3D(double x, double y, double z){
         this.x = x;
